db7e8565a715db71016107f4321ca10a2d669b02
Captain Isaac A <87286968+CaptainIsaaca@users.noreply.github.com>
2022-10-24 20:34:02 +0200

    Tauri

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,18 +2,50 @@
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
+use tauri::Manager;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!!!!", name)
 }
+#[tauri::command]
+async fn close_splashscreen(window: tauri::Window) {
+    let splashscreen_window = window.get_window("splashscreen").unwrap();
+    let main_window = window.get_window("main").unwrap();
+    tauri::async_runtime::spawn(async move {
+        std::thread::sleep(std::time::Duration::from_secs(3));
+        splashscreen_window.close().unwrap();
+        main_window.show().unwrap();
+    });
+}
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, close_splashscreen])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
-
-    println!("Hello")
 }
+
+// fn main() {
+//     tauri::Builder::default()
+//       .setup(|app| {
+//         let splashscreen_window = app.get_window("splashscreen").unwrap();
+//         let main_window = app.get_window("main").unwrap();
+//         // we perform the initialization code on a new task so the app doesn't freeze
+//         tauri::async_runtime::spawn(async move {
+//           // initialize your app here instead of sleeping :)
+//           println!("Initializing...");
+//           std::thread::sleep(std::time::Duration::from_secs(3));
+//           println!("Done initializing.");
+  
+//           // After it's done, close the splashscreen and display the main window
+//           splashscreen_window.close().unwrap();
+//           main_window.show().unwrap();
+//         });
+//         Ok(())
+//       })
+//       .run(tauri::generate_context!())
+//       .expect("failed to run app");
+//   }
+
---
+++ src/components/Titlebar.vue
@@ -0,0 +1,53 @@
+<template>
+    <div data-tauri-drag-region class="titlebar">
+      <div @click="minimizeWindow" class="titlebar-button" id="titlebar-minimize">
+        <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 12H4"></path></svg>
+      </div>
+      <div @click="closeWindow" class="titlebar-button-red" id="titlebar-close">
+        <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
+      </div>
+    </div>
+</template>
+
+<script>
+import { appWindow } from '@tauri-apps/api/window'
+
+    export default {
+        methods: {
+            minimizeWindow() {
+                appWindow.minimize()
+            },
+            closeWindow() {
+                appWindow.close()
+            }
+        }
+    }
+</script>
+
+<style scoped>
+.titlebar {
+  height: 30px;
+  /* background: #329ea3; */
+  user-select: none;
+  display: flex;
+  justify-content: flex-end;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+}
+.titlebar-button, .titlebar-button-red {
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  width: 36px;
+  height: 30px;
+  cursor: pointer;
+  color: rgba(255,255,255,.85);
+}
+.titlebar-button:hover {
+  background: rgba(255,255,255,.1);
+}
+.titlebar-button-red:hover {
+  background: rgb(212, 77, 77);
+}</style>
