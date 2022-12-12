42f495dc61cc4babbf41aa2b5774b8a19d872232
elrakabawi <mselrakabawi@gmail.com>
2022-12-12 15:30:32 +0200

    Client downloader functionality

--- server/index.cjs
+++ server/index.cjs
@@ -11,9 +11,11 @@ const serverPort = 6212;
 const websocketPort = 6213;
 const server = http.createServer();
 const websocketServer = new WebSocket.Server({ server });
+const Downloader = require("nodejs-file-downloader");
 // server.on('request', app);
 
 websocketServer.on("connection", (webSocketClient) => {
+    console.log(`Client connected at ${new Date}`)
     // send feedback to the incoming connection
     webSocketClient.on('message', async (message) => {
             message = "" + message // weird, but works!
@@ -23,9 +25,59 @@ websocketServer.on("connection", (webSocketClient) => {
                 let assetsURL = []
                 for(let asset in assets) {
                     totalSize += assets[asset].size
-                    assetsURL.push(assets[asset].browser_download_url)
+                    let assetArr = []
+                    assetArr.push(assets[asset].browser_download_url)
+                    assetArr.push(assets[asset].name)
+                    assetArr.push(assets[asset].size)
+                    assetsURL.push(assetArr)
                 }
-                
+                let totalDownloaded = 0
+                let totalSwap = 0
+                for(let asset in assetsURL) {
+                    // some crazy fucking string manipulation to format name and path
+                    let url = assetsURL[asset][0]
+                    let name = assetsURL[asset][1]
+                    let size = assetsURL[asset][2]
+                    name = name.split('@').join('\\')
+                    let path = `${process.cwd()}\\Client\\${name}`
+                    name = name.split('\\')
+                    name = name[name.length-1]
+                    path = path.split('\\')
+                    path.pop()
+                    path = path.join('\\')
+                    let fileDownloaded = 0
+                    const downloader = new Downloader({
+                        url,
+                        directory: path,
+                        fileName: name,
+                        cloneFiles: false,
+                        maxAttempts: 3, // failed download repeats
+                        onProgress: function (percentage, chunk, remainingSize) {
+                            if(remainingSize === 0) {
+                                totalDownloaded += totalSwap
+                            } else {
+                                fileDownloaded = (size - remainingSize)
+                                totalSwap = fileDownloaded
+                            }
+                            console.table(["size", size], ["remainingSize", remainingSize], ["fileDownloaded", fileDownloaded], ["totalSwap", totalSwap])
+                            //Gets called with each chunk.
+                            console.log("% ", percentage);
+                            console.log("Current chunk of data: ", chunk);
+                            console.log("Remaining bytes: ", remainingSize);
+
+
+                            webSocketClient.send(JSON.stringify({ totalSize, totalDownloaded }))
+                        },
+                        onError: function (error) {
+                            console.log("Error from attempt ", error);
+                        },
+                    })
+                    try {
+                        await downloader.download();
+                    } catch (error) {
+                        console.log(error);
+                    }
+                }
                 webSocketClient.send(JSON.stringify({ assetsURL, totalSize }))
             // setInterval(() => {
             //   let time = new Date();
@@ -53,18 +105,3 @@ async function getClientReleaseAssets() {
         repo: 'FC-Client-Binaries'
     }))
 }
-
-
-// const options = {
-//   folders: { exclude: ['.*', 'node_modules', 'test_coverage'] },
-//   files: { include: ['*.Fbx', '*.json', 'png', 'jpg'] },
-// };
-
-// hashElement('../src-tauri/Windows')
-//   .then(hash => {
-//     console.log(hash.toString());
-//     console.log('hello')
-//   })
-//   .catch(error => {
-//     return console.error('hashing failed:', error);
-//   });"use strict";
