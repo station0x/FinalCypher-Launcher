ff8b2cb6cebe326b4cc6c8fe132e60a8435cc0cc
elrakabawi <mselrakabawi@gmail.com>
2022-12-22 07:52:06 +0200

    social media cta + splash titlebar fix

--- src/components/Titlebar.vue
+++ src/components/Titlebar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div data-tauri-drag-region class="titlebar">
+    <div v-if="!$store.state.isSplash" data-tauri-drag-region class="titlebar">
       <div @click="minimizeWindow" class="titlebar-button" id="titlebar-minimize">
         <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 12H4"></path></svg>
       </div>
